81d03d63c2d34ca68a2f8d6fded3805c79fdc7f3
Chris Eldredge <chris.eldredge@gmail.com>
2012-11-09 17:11:13 -0500

    Set major version to 3.0 to follow Lucene.Net library version.

--- psake.ps1
+++ psake.ps1
@@ -26,7 +26,7 @@ if (!$buildNumber) {
 }
 
 if (!$properties["version"]) {
-	$properties["version"] = "1.0.$buildNumber"
+	$properties["version"] = "3.0.$buildNumber"
 }
 if (!$properties["revision"]) {
 	$properties["revision"] = $revision
